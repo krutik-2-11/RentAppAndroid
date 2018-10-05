7fd3903c6096c9d15ddefd8eee5bcdd51196e2b3
Krutik Pathak <krutikpathak180@gmail.com>
2018-10-05 17:52:29 +0530

    Login and Registration with helper java class

---
+++ app/src/main/java/com/example/kittu/to_let/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.kittu.to_let;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Button button_login;
+    private static Button button_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LoginButton();
+        RegisterButton();
+    }
+
+
+    public void LoginButton()
+    {
+        button_login = (Button)findViewById(R.id.btnLogin);
+
+        button_login.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".Login");
+                        startActivity(intent);
+
+                    }
+
+
+
+                }
+        );
+    }
+
+
+
+
+    public void RegisterButton()
+    {
+        button_register = (Button)findViewById(R.id.btnRegister);
+
+        button_register.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".Register");
+                        startActivity(intent);
+
+                    }
+
+
+
+                }
+        );
+    }
+}
